32137b206a35fff70c752b32b0bfdc498e21dc24
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-10-22 12:19:10 +0700

    Percobaan 1

---
+++ SiakadFor18.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SiakadFor18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nilai, terendah=100, tertinggi=0;
+        for (int i=1 ; i<=10 ; i++){
+            System.out.print("Masukkan nilai mahasiswa ke "+i+" : ");
+            nilai = sc.nextInt();
+            if (nilai>tertinggi) {
+                tertinggi=nilai;
+            }
+            if (nilai<terendah) {
+                terendah=nilai;
+            }
+        }
+        System.out.println("Nilai tertinggi : "+tertinggi);
+        System.out.println("Nilai terendah  :"+terendah);
+    }
+}
