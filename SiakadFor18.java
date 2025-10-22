fa70b9087e0ed535090a5dd821d1721cf160c51c
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-10-22 12:55:29 +0700

    modifikasi, pertanyaan 4 percobaan 1

--- SiakadFor18.java
+++ SiakadFor18.java
@@ -4,6 +4,7 @@ public class SiakadFor18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, terendah=100, tertinggi=0;
+        int mhslulus=0, mhstidaklulus=0;
         for (int i=1 ; i<=10 ; i++){
             System.out.print("Masukkan nilai mahasiswa ke "+i+" : ");
             nilai = sc.nextInt();
@@ -13,8 +14,17 @@ public class SiakadFor18 {
             if (nilai<terendah) {
                 terendah=nilai;
             }
+
+            if (nilai>=60) {
+                mhslulus++;
+            } else {
+                mhstidaklulus++;
+            }
         }
+
         System.out.println("Nilai tertinggi : "+tertinggi);
-        System.out.println("Nilai terendah  :"+terendah);
+        System.out.println("Nilai terendah  : "+terendah);
+        System.out.println("Jumlah mahasiswa lulus       = "+mhslulus);
+        System.out.println("Jumlah mahasiswa tidak lulus = "+mhstidaklulus);
     }
 }
