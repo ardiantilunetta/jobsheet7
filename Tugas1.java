2f8895381f05771c7191142217249a4725c9cc5b
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-10-24 19:05:00 +0700

    deklarasi variabel

---
+++ Tugas1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hargatiket=50000, pelanggan, harga, totaltiket, totalpenjualan;
+        String ulang;
+        
+        do {
+            
+        } while (ulang=="ya");
+    }
+}
