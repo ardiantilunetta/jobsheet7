73637d9b8928cce8741904f99be9a046c637f4c0
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2025-10-24 19:13:13 +0700

    input negatif

--- Tugas1.java
+++ Tugas1.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public class Tugas1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int hargatiket=50000, pelanggan, harga, totaltiket, totalpenjualan;
+        int hargatiket=50000, pelanggan, totaltiket;
+        double harga, totalpenjualan;
         String ulang;
         
         do {
-            
-        } while (ulang=="ya");
+            System.out.print("Masukkan jumlah tiket : ");
+            pelanggan = sc.nextInt();
+
+            if (pelanggan < 0) {
+                System.out.println("Input tidak valid! Masukkan ulang jumlah tiket.\n");
+                continue;
+            }
+
+        } while (true);
     }
 }
